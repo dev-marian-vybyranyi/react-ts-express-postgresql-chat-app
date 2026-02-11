f94a1589c0fc72724527aa5dacec76709d33650d
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-11 11:02:23 +0200

    Remove Prisma Neon adapter and downgrade Prisma to version 5.14.0.

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool } from "@neondatabase/serverless";
 
-const connectionString = process.env.DATABASE_URL!;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
