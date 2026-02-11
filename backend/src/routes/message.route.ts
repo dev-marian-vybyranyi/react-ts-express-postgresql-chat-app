7bdda1a0a60ae9b08e0f3b79f93bf7a9d4ba4028
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-11 11:51:22 +0200

    Add an endpoint and controller to fetch users for the sidebar, excluding the authenticated user.

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   getMessages,
+  getUsersForSidebar,
   sendMessage,
 } from "../controllers/message.controller.js";
 import protectRoute from "../middlewares/protectRoute.js";
 const router = express.Router();
 
+router.get("/conversations", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
 
