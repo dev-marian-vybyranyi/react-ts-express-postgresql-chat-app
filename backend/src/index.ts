6cdfca953664040495cd6ffc0e47b085f4971342
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-11 11:20:53 +0200

    Add `protectRoute` middleware, `getMe` endpoint, and integrate `cookieParser` for user authentication.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
