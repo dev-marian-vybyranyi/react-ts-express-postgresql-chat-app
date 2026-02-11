4fb0fc4e7c84181ffab6b598afe0c68066cb50aa
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-11 11:42:55 +0200

    Add message sending functionality with a new controller and protected route.

---
+++ backend/src/controllers/message.controller.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
+
+export const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params as { id: string };
+    const senderId = req.user.id;
+
+    let conversation = await prisma.conversation.findFirst({
+      where: {
+        participantIds: {
+          hasEvery: [senderId, receiverId],
+        },
+      },
+    });
+
+    if (!conversation) {
+      conversation = await prisma.conversation.create({
+        data: {
+          participantIds: {
+            set: [senderId, receiverId],
+          },
+        },
+      });
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        senderId,
+        body: message,
+        conversationId: conversation.id,
+      },
+    });
+
+    if (newMessage) {
+      conversation = await prisma.conversation.update({
+        where: {
+          id: conversation.id,
+        },
+        data: {
+          messages: {
+            connect: {
+              id: newMessage.id,
+            },
+          },
+        },
+      });
+    }
+
+    res.status(201).json(newMessage);
+  } catch (error: any) {
+    console.error("Error in sendMessage: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,9 +1,8 @@
 import express from "express";
-
+import { sendMessage } from "../controllers/message.controller.js";
+import protectRoute from "../middlewares/protectRoute.js";
 const router = express.Router();
 
-router.post("/conversation", (req, res) => {
-  res.send("Conversation");
-});
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
